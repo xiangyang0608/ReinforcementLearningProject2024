49cb730bcc93b61630f1bb72b8b2c6e74df84f77
xiangyang0608 <hongyangye@163.com>
2024-03-18 08:54:03 +0100

    upload grid world with obstacles

--- Utils/envVisual.py
+++ Utils/envVisual.py
@@ -5,7 +5,7 @@ class EnvironmentVisualizer:
     def __init__(self, environment):
         self.environment = environment
 
-    def visualize(self):
+    def visualize(self, type=None):
         # Mark start and goal positions
         grid = np.zeros((self.environment.rows, self.environment.cols, 3))  # RGB grid
 
@@ -15,21 +15,26 @@ class EnvironmentVisualizer:
         grid[start_row, start_col] = [0, 1, 0]  # Green for start position
         grid[goal_row, goal_col] = [1, 0, 0]    # Red for goal position
 
-        # Mark wind effect
-        if self.environment.wind is not None:
-            for col, wind_strength in enumerate(self.environment.wind):
-                if wind_strength != 0:
-                    arrow_direction = (0, 1)  # Wind direction (up or down)
-                    arrow_length = abs(wind_strength) # Wind strength (arrow length)
-                    arrow_start = (1, col)  # Arrow starting point (above the grid)
-                    arrow_color = (0, 0, 0)  # Black color for arrows
-                    plt.arrow(arrow_start[1], arrow_start[0], *arrow_direction, width=0.05, head_width=0.3, head_length=0.3 * arrow_length, color=arrow_color)
+        if type == 'wind':
+            # Mark wind effect
+            if self.environment.wind is not None:
+                for col, wind_strength in enumerate(self.environment.wind):
+                    if wind_strength != 0:
+                        arrow_direction = (0, 1)  # Wind direction (up or down)
+                        arrow_length = abs(wind_strength) # Wind strength (arrow length)
+                        arrow_start = (1, col)  # Arrow starting point (above the grid)
+                        arrow_color = (0, 0, 0)  # Black color for arrows
+                        plt.arrow(arrow_start[1], arrow_start[0], *arrow_direction, width=0.05, head_width=0.3, head_length=0.3 * arrow_length, color=arrow_color)
 
-                    for row in range(self.environment.rows):
-                        grid_color = [0.8, 0.9, 1 - wind_strength / max(self.environment.wind)]
-                        grid[row, col] = grid_color
-
-
+                        for row in range(self.environment.rows):
+                            grid_color = [0.8, 0.9, 1 - wind_strength / max(self.environment.wind)]
+                            grid[row, col] = grid_color
+        
+        if type == 'obstacle':
+            # Mark the obstacles in grey
+            for obstacle in self.environment.obstacles:
+                obstacle_row, obstacle_col = obstacle
+                grid[obstacle_row, obstacle_col] = [0.5, 0.5, 0.5]
 
 
         # Mark current position if not None
